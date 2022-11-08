e4ed9e352dbc17eb0d11770dd3bcb0204998ec52
Steven Roberts <sr320@uw.edu>
2022-11-08 10:50:06 -0800

    added 1 mm filter using pipes

--- script/fish-analysis.R
+++ script/fish-analysis.R
@@ -12,8 +12,12 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 #create new column based on size of fish length
 fish_data_cat <- fish_data %>%
+  filter(scalelength > 1) %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
 #need to remove to remove the fish with a scalelength of less than 1 mm
 fish_data_cat_fixed <- fish_data_cat %>%
   filter(scalelength > 1)
+
+
+
