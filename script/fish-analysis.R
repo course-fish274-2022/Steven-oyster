7b2e223379bb8aaa75bb1cdf816f76ba74a45aef
Steven Roberts <sr320@uw.edu>
2022-11-08 10:48:21 -0800

    fixed data set to exclude scale length <1

--- script/fish-analysis.R
+++ script/fish-analysis.R
@@ -13,3 +13,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 #create new column based on size of fish length
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+#need to remove to remove the fish with a scalelength of less than 1 mm
+fish_data_cat_fixed <- fish_data_cat %>%
+  filter(scalelength > 1)
