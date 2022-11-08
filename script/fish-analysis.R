17cc45b1a76f8ef104e2bfce31f91423057876a2
Steven Roberts <sr320@uw.edu>
2022-11-08 10:39:31 -0800

    added / improved comment describing table

--- script/fish-analysis.R
+++ script/fish-analysis.R
@@ -10,7 +10,6 @@ library(dplyr)
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 
-#create new column based on size
-
+#create new column based on size of fish length
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
